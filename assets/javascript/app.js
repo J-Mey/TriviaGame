ce785c2fb8434c4774f356f8637b8d8664bcdac7
J <j.mey.r8@gmail.com>
2019-05-20 22:57:58 -0400

    pseudocoded remaining parts left to finish

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
+// button click to start game
     $("button").on("click", gameStart);
+
+    // hides button when clicked so user can not click it again
     $("#start").click(function (){
         $(this).hide();
     })
@@ -29,37 +32,36 @@ function decrement() {
 }
 
 // Question List
-var quizList = $("#quiz-box");
 
 var questions = [
     {
         question: "Whos is the strongest on this list?",
         answers: ["Goku ", "Hit ", "Beerus ", "Whis "],
-        correct: "3"
+        correct: "Whis "
     },
 
     {
         question: "What is Goku's go to move?",
         answers: ["Spirit Bomb ", "Kamehameha ", "Instant Transmission ", "Solar Flare "],
-        correct: "1"
+        correct: "Kamehameha "
     },
 
     {
         question: "Who is Goku's best friend?",
         answers: ["Vegeta ", "Piccolo ", "Krillin ", "Tien "],
-        correct: "2"
+        correct: "Krillin "
     },
 
     {
         question: "What is the name of earth's dragon?",
         answers: ["Porunga ", "Shenron ", "Zamasu ", "Champa "],
-        correct: "1"
+        correct: "Shenron "
     },
 
     {
         question: "Which color is the first super saiyan form?",
         answers: ["Blue ", "Red ", "Green ", "Yellow "],
-        correct: "3"
+        correct: "Yellow "
     },
 ]
 
@@ -68,51 +70,29 @@ $("#start").on("click", function() {
 
     // Loops through 5 questions
     for (var i=0; i < questions.length; i++) {
-        $(quizList).append("<h2>" + questions[i].question + "</h2>");
-        //$("<h4>" + questions[i].question + "</h4>").appendTo(quizList);
-        //quizList.append("<h3>" + questions[i].question + "</h3>");
-    
+        $("#quiz-box").append("<h2>" + questions[i].question + "</h2>");
+        
         // Loops through answers for radio buttons
         for (var j=0; j < questions[i].answers.length; j++) {
-            $(quizList).append("<input type='radio' name='question" + i +
+
+            // Takes answer array and appends to a radio button
+            $("#quiz-box").append("<input type='radio' name='question" + i +
             "'value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
         }
     }
 
-    //var newButton = $("<button id='stop-button' type='button' class='btn btn-default'>DONE</button>");
-    //$("#quiz-box").append(newButton);
+    // adding a finish button
+    var newButton = $("<button type='button class='btn btn=primary btn-lg id='stop'>Finish</button>");
+    $("#quiz-box").append(newButton);
 });
 
-/*var correctAnswer = 0;
-var incorrectAnswer = 0;
 
-$(document).on("click", resultsCheck)
+// function to check through loop for correct answers from the questions array
 
-function resultsCheck () {
-    var userAnswer = $('input:check').val()
-    console.log(userAnswer);
+// function to keep score of how many correct and incorrect answers
 
-    if(userAnswer === questions[i].correct){
-        correctAnswer ++;
-        console.log(correctAnswer);
-        console.log(correct)
-    }
-} 
- for (var i=0; i < questions.length; i++) {
-      if ($(quizList).val() === questions[i].correct) {
-        correctAnswer++;  
-        console.log ("good" + correctAnswer);
-          
+// Once timer runs out, a page will show the results of the quiz and append it to the id=results html
 
-      } else {
-          incorrectAnswer++;
-          console.log ("wrong");
-          console.log(quizList);
-          console.log(incorrectAnswer);
-      }
-
-
-      }*/
   
 
   
